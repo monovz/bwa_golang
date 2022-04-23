8871123baf9b8bc224c0d77e335dcfc74b37f117
monovz <novritsazulfikar@gmail.com>
2022-04-24 01:45:32 +0700

    add cors and create API fetch user

--- handler/user.go
+++ handler/user.go
@@ -162,3 +162,14 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	response := helper.APIResponse("Successfully upload avatar", http.StatusOK, "success", data)
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) FetchUser(c *gin.Context) {
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	formatter := user.FormatUser(currentUser, "")
+
+	response := helper.APIResponse("Successfully fetch user data", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
